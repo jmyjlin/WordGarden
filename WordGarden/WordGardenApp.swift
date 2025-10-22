81a7ad0ffcd705de6b37dc101549911d365a9e49
Jimmy Lin <jmyjlin@gmail.com>
2025-10-22 01:45:42 -0700

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Jimmy Lin on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
